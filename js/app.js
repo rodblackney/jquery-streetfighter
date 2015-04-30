3100f389922aee929b7eb739c411768cc36ef5df
Rod Blackney <rblackney@gmail.com>
2015-04-30 22:26:23 +1000

    added sound and animation

--- js/app.js
+++ js/app.js
@@ -1,29 +1,33 @@
-$(document).ready(function() {
-  	// ryu is in his ready to fight position
-	$('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
-  })
+$(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    })    
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
 
-  .mouseleave(function() {
-  	// ryu mouseleave
-    $('.ryu-ready').hide();
-    $('.ryu-still').show();
-  })
-
-  .mousedown(function() {
-  	// ryu mousedown
-    console.log('mousedown');
-    // play hadouken sound
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').show();
-    // show hadouken and animate it to the right of the screen
-  })
-
-  .mouseup(function() {
-    // ryu goes back to his ready position
-  	$('ryu-throwing').hide();
-  	$('ryu-ready').show();
-  });
 });
+
+function playHadouken() {
+        $('#hadouken-sound')[0].volume = 0.5;
+        $('#hadouken-sound')[0].load();
+        $('#hadouken-sound')[0].play();
+    }
