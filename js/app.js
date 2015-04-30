aad821e6821fa9f03752b56c07c0737a090a3752
Rod Blackney <rblackney@gmail.com>
2015-04-30 23:05:24 +1000

    added keydown function

--- js/app.js
+++ js/app.js
@@ -1,33 +1,66 @@
-$(document).ready(function(){
-    $('.ryu').mouseenter(function(){
-        $('.ryu-still').hide();
-        $('.ryu-ready').show();
+$(document).ready(function() {
+    $(".ryu").mouseenter(function() {
+        $(".ryu-still").hide();
+        $(".ryu-cool").hide();
+        $(".ryu-ready").show();
     })
-    .mouseleave(function() {
-        $('.ryu-still').show();
-        $('.ryu-ready').hide();
-    })
-    .mousedown(function(){
-        playHadouken();
-        $('.ryu-ready').hide();
-        $('.ryu-throwing').show();
-        $('.hadouken').finish().show().animate(
-            {'left': '1020px'},
-            500,
-            function(){
-                $(this).hide();
-                $(this).css('left', '520px');
-            });
-    })    
-    .mouseup(function(){
-        $('.ryu-throwing').hide();
-        $('.ryu-ready').show();
-    });
 
+    .mouseleave(function() {
+        $(".ryu-ready").hide();
+        $(".ryu-cool").hide();
+        $(".ryu-still").show();
+    })
+
+// ryu throws hadouken
+
+    .mousedown(function() {
+        playHadouken();
+        $(".ryu-ready").hide();
+        $(".ryu-cool").hide();
+        $(".ryu-still").hide();
+        $(".ryu-throwing").show();
+        $('.hadouken').finish().show()
+        .animate(
+        {'left': '1020px'}, 500,
+        function() {
+        $(this).hide();
+        $(this).css('left', '630px');
+    }
+  );
+
+    })
+
+    .mouseup(function() {
+        $(".ryu-throwing").hide();
+        $(".ryu-cool").hide();
+        $(".ryu-still").hide();
+        $(".ryu-ready").show();
+    })
+
+// ryu acting cool
+
+    $("body").keydown(function(e) {
+        if (e.which == 88) {
+            $(".ryu-still").hide();
+            $(".ryu-ready").hide();
+            $(".ryu-cool").show();
+            $(".hadouken").hide();
+        }
+    })
+
+    $("body").keyup(function(e) {
+        if (e.which == 88) {
+            $(".ryu-still").show();
+            $(".ryu-ready").hide();
+            $(".ryu-cool").hide();
+            $(".hadouken").hide();
+
+        }
+    })
 });
 
 function playHadouken() {
-        $('#hadouken-sound')[0].volume = 0.5;
-        $('#hadouken-sound')[0].load();
-        $('#hadouken-sound')[0].play();
-    }
+    $("#hadouken-sound")[0].volume = 0.5;
+    $("#hadouken-sound")[0].load();
+    $("#hadouken-sound")[0].play();
+}
